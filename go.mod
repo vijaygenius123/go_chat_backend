1d68239aadd0eaabfac6f98c9781983728492103
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-01-03 17:58:01 +0530

    HTTP Setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_chat_backend
+
+go 1.14
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func setupRoutes() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "Hello Chat")
+	})
+}
 
 func main() {
 	fmt.Printf("Hello Chat")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
